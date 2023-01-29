49dd5fb4a3034e15d3b210ae63a6154038212778
PaulusSun <123460451+PaulusSun@users.noreply.github.com>
2023-01-29 14:45:25 +0300

    Add files via upload

---
+++ 28 19_29_5763d577b5bec110.87213797lesson_1_task_7.py
@@ -0,0 +1,32 @@
+def print_greeting1():
+    print(1)
+def print_greeting2():
+    print(2)
+def print_greeting3():
+    print(3)
+def print_greeting4():
+    print(4)
+def print_greeting5():
+    print(5)
+def print_greeting6():
+    print(6)
+def print_greeting7():
+    print(7)
+def print_greeting8():
+    print(8)
+def print_greeting9():
+    print(9)
+def print_greeting0():
+    print(0)
+print_greeting8()
+print_greeting8()
+print_greeting0()
+print_greeting0()
+print_greeting5()
+print_greeting5()
+print_greeting5()
+print_greeting3()
+print_greeting5()
+print_greeting3()
+print_greeting5()
+
---
+++ 28 19_29_5763d577b5bf2a99.28857976lesson_1_task_5.py
@@ -0,0 +1,6 @@
+my_age = input("Ваш возраст: ")
+print("Ваш возраст: "+my_age)
+my_age = int(my_age)
+my_age = my_age+1
+my_age = str(my_age)
+print("Ваш новый возраст: "+my_age)
